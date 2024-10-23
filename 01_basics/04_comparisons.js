67e1c68acf83694f93774499565ab21a2f67ba15
nandancode <49531806+nandancode@users.noreply.github.com>
2024-10-23 16:19:33 +0000

    datatype Summary

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,17 @@
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+
+console.log(undefined == 0)
+
+
+/ === 
+/prefer to use this as it also checks the data types
+
+
+
+
