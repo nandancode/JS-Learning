46d4908ea5d61411fda1f9760d80bbaf1decdca5
nandancode <49531806+nandancode@users.noreply.github.com>
2024-10-26 07:17:44 +0000

    arrays part 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,40 @@
+const myArr = [1, 2, 3, 4 ,5]
+
+const myArr2 = new Array(1, 2, 3,4, 5)
+
+// console.log(myArr)
+// console.log(myArr2)
+
+
+// myArr.push(6)
+
+// console.log(myArr)
+
+// myArr.pop()
+
+// console.log(myArr)
+
+
+// myArr.unshift(9)
+
+// console.log(myArr)
+
+// myArr.shift()
+
+// console.log(myArr)
+
+
+// console.log(myArr.includes(9))
+
+// console.log(myArr.indexOf(4))
+
+
+console.log('A' , myArr)
+const myn1 = myArr.slice(1, 3)
+console.log(myn1)
+console.log('B', myArr)
+
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2)
+console.log('C', myArr)
