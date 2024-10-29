6c36c1cd42bd7a1c2e4d0bff59b85369a08c0b0d
nandancode <49531806+nandancode@users.noreply.github.com>
2024-10-29 12:01:52 +0000

    switch case

---
+++ 04_control_flow/two.js
@@ -0,0 +1,16 @@
+const month = "jan"
+
+switch(month){
+    case "jan":
+        console.log("January")
+        break
+    case "feb":
+        console.log("february")
+        break;
+    case "march":
+        console.log("march")
+        break;
+    default:
+        console.log("default case")
+
+}
