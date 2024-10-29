513a448532777d5859e100a8da84662aca80daf9
nandancode <49531806+nandancode@users.noreply.github.com>
2024-10-29 11:51:08 +0000

    if-else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,15 @@
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if(temperature <= 50){
+//     console.log("temperature is less than 50")
+// }
+// else{
+//     console.log("temperature is greater than 50")
+// }
+
+// console.log("execute")
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+// elseif and 'and' 'or' are same as other languages
